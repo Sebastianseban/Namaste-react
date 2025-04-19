88eeb728d3b3cb1eeb8dcf16c11c3cbf6072a3af
sebastian <sebastianseban0105@gmail.com>
2025-04-19 22:49:03 +0530

    added filter

--- Episode05 - Let's get Hooked/src/components/Body.jsx
+++ Episode05 - Let's get Hooked/src/components/Body.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import RestaurantCard from './RestaurantCard';
 import restaurantList from '../utils/mockData';
-
+import { useState } from 'react';
 
 
 const Body = () => {
+    const [restaurent, setRestaurent] = useState(restaurantList)
+
+    
     return (
       <div className="body">
         <div className="search-box">
           <input placeholder="search" />
           {/* <CiSearch className="search-icon" /> */}
         </div>
+        <div className='filter'>
+            <button
+
+            onClick={()=>{
+                const filteredList = restaurantList.filter((res)=> res.info.avgRating > 4);
+                setRestaurent(filteredList)
+            }}
+            
+            className='filter-btn'>Top Rated Restaurant</button>
+        </div>
         <div className="restaurant-container">
-          {restaurantList.map((restaurant) => (
+
+          {restaurent.map((restaurant) => (
             <RestaurantCard
               key={restaurant.info.id}
               restaurantData={restaurant}
