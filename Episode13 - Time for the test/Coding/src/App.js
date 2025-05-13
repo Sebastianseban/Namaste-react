1ff00bbfb346d99bbe710ece307e6d94ba5753d9
sebastian <sebastianseban0105@gmail.com>
2025-05-13 11:34:56 +0530

    added class  code

--- Episode13 - Time for the test/Coding/src/App.js
+++ Episode13 - Time for the test/Coding/src/App.js
@@ -1,107 +1,85 @@
-import React, { lazy, Suspense, useEffect, useState } from "react";
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Contact from "./components/Contact";
-import Error from "./components/Error";
-import RestaurantMenu from "./components/RestaurantMenu";
-import Login from "./components/Login";
-import Cart from "./components/Cart";
-import { RestaurantShimmer } from "./components/Shimmer";
-import UserContext from "./context/UserContext";
-import { Provider } from "react-redux";
-import appStore from "./store/appStore";
-import "./index.css";
+import React, { lazy, Suspense } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
 
-/**
- * Chunking
- * Lazy Loading
- * Code Splitting
- * Dynamic Imports
- * Dynamic Bundding
- * Prefetching
- * Suspense
- * On-Demand Loading
- */
+import Header from './components/Header';
+import Body from './components/Body';
+// import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import RestaurantMenu from './components/RestaurantMenu';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import UserContext from './utils/UserContext';
+import { Provider } from 'react-redux';
+import appStore from './utils/appStore';
+import Cart from './components/Cart';
 
-const Body = lazy(() => import("./components/Body"));
-const About = lazy(() => import("./components/About"));
+const Grocery = lazy(() => import('./components/Grocery'));
 
-const App = () => {
+const AppLayout = () => {
   const [userName, setUserName] = useState();
 
-  // Authentication Logic
+  // Authentication
   useEffect(() => {
-    // Make an API Call and send username and password
+    // Make an API call and send username and password
     const data = {
-      name: "Guest User",
+      name: 'Vasu K',
     };
-
     setUserName(data.name);
   }, []);
 
   return (
     <Provider store={appStore}>
       <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-        <div className="w-full flex flex-col justify-between items-center mt-[120px] min-h-[calc(100vh-120px)]">
+        <div className="app">
           <Header />
           <Outlet />
-          <Footer />
         </div>
       </UserContext.Provider>
     </Provider>
   );
 };
 
-// Children Routes
 const appRouter = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/',
+    element: <AppLayout />,
     children: [
       {
-        path: "/",
-        element: (
-          <Suspense fallback={<RestaurantShimmer />}>
-            <Body />
-          </Suspense>
-        ),
+        path: '/',
+        element: <Body />,
       },
       {
-        path: "/about",
-        element: (
-          <Suspense
-            fallback={<h1 className="text-3xl font-bold">Loading...</h1>}
-          >
-            <About />
-          </Suspense>
-        ),
+        path: '/about',
+        element: <About />,
       },
       {
-        path: "/contact",
+        path: '/contact',
         element: <Contact />,
       },
       {
-        path: "/restaurants/:resId",
+        path: '/grocery',
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+      {
+        path: '/restaurants/:resId',
         element: <RestaurantMenu />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
-      }
+      },
     ],
     errorElement: <Error />,
   },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/hello",
-    element: <h1 className="text-3xl font-bold">Hello, World!!</h1>,
-  },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<RouterProvider router={appRouter} />);
--- Episode13 - Time for the test/Coding/src/components/About.js
+++ Episode13 - Time for the test/Coding/src/components/About.js
@@ -1,66 +1,64 @@
-import { Component } from "react";
-import burgerImage from "../../../../public/images/burger-image.png";
-import ProfileClass from "./ProfileClass";
-import UserContext from "../context/UserContext";
+import { Component } from 'react';
+// import User from './User';
+import UserClass from './UserClass';
+import UserContext from '../utils/UserContext';
 
 class About extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showProfile: false,
-    };
+    // console.log('Parent Constructor');
+  }
+
+  componentDidMount() {
+    // console.log('Parent Component Did Mount');
   }
 
   render() {
-    const { showProfile } = this.state;
+    // console.log('Parent Render');
 
     return (
-      <div className="w-[90%] flex flex-col justify-center items-center flex-wrap gap-[40px]">
-        <div className="flex flex-col justify-center items-center flex-wrap gap-[40px] transition-all ease-in-out delalay-[0.3s]">
-          {
-            <button
-              className="text-white bg-[#d97919] text-[1.2rem] font-semibold py-2.5 px-5 border-none outline-none rounded-[5px] cursor-pointer hover:bg-[darkgreen] transition-all 0.3s ease-in-out"
-              onClick={() => {
-                this.setState({ showProfile: !showProfile });
-              }}
-            >
-              {showProfile ? "Hide My Profile" : "Show My Profile"}
-            </button>
-          }
-          {showProfile && (
-            <ProfileClass name={"Bharat Kumar"} location={"Bihar"} />
-          )}
+      <div className="about-page">
+        <h1>About Class Component</h1>
+        <div>
+          LoggedInUser
+          <UserContext.Consumer>
+            {({ loggedInUser }) => <h1>{loggedInUser}</h1>}
+          </UserContext.Consumer>
         </div>
-
-        <div className="flex items-center justify-between flex-wrap gap-[40px] w-full">
-          <div className="about-left">
-            <h1 className="text-[60px] text-[#494949] font-bold leading-[70px]">
-              <UserContext.Consumer>
-                {({ loggedInUser }) => {
-                  return (
-                    <div>
-                      Hi, {loggedInUser ? loggedInUser.slice(0, 13) : ""}
-                    </div>
-                  );
-                }}
-              </UserContext.Consumer>
-              Welcome to <br /> The world of <br />
-              <span className="text-white bg-[#d97919] rounded-[15px] px-[15px] py-[5px]">
-                Tasty & Fresh Food
-              </span>
-            </h1>
-            <h4 className="text-[22px] mt-[10px] italic font-semibold">
-              "Better you will feel if you eat a Tasty
-              <span className="text-[#d97919]">Trails</span> healthy meal"
-            </h4>
-          </div>
-          <div>
-            <img src={burgerImage} alt="Food Image" />
-          </div>
-        </div>
+        <h2>This is About Page</h2>
+        <UserClass name={'First'} location={'Badvel class'} />
       </div>
     );
   }
 }
+
+// * RENDER CYCLE OF CLASS BASED COMPONENTS WHEN THE CLASS HAS TWO CHLIDREN
+
+/* 
+*  - Parent Constructor()              -- Render Phase
+*  - Parent Render()
+
+*    - First Child Constructor()
+*    - First Child Render()
+*                                      -- Render Phase
+*    - Second Child Constructor()
+*    - Second Child Render()
+
+*     <DOM UPDATED - IN SINGLE BATCH> -> Optimizes the Performance of App  -- Commit Phase
+*    - First Child ComponentDidMount()
+*    - Second Child ComponentDidMount()
+
+*  - Parent ComponentDidMount()=
+*/
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Us</h1>
+//       <h2>This is About Page</h2>
+//       <UserClass name={'Vasu (class)'} location={'Badvel class'} />
+//     </div>
+//   );
+// };
 
 export default About;
